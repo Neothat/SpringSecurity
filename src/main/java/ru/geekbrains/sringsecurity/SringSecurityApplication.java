80083890525bcc73c87450f0c963adb1162f57d3
Denis <neothat@mail.ru>
2021-12-23 02:58:38 +0300

    Initial commit

---
+++ src/main/java/ru/geekbrains/sringsecurity/SringSecurityApplication.java
@@ -0,0 +1,13 @@
+package ru.geekbrains.sringsecurity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SringSecurityApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SringSecurityApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ru/geekbrains/sringsecurity/SringSecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.geekbrains.sringsecurity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SringSecurityApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
